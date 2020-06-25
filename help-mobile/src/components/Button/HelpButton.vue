0fcc9721ef54b8d3f2ca1622c1e3e0c848e029e6
RamosEduardo <ramoscleitoneduardo@gmail.com>
2020-06-24 22:04:14 -0300

    Feat: create home space

---
+++ help-mobile/src/components/Button/HelpButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <button class="button" icon="mail" :label="label" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: { type: String, default: '' },
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ help-mobile/src/pages/Connected/Pages/Home.vue
@@ -0,0 +1,69 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card
+        class="my-card"
+        flat bordered
+        v-for="(value, index) in list" :key="index"
+      >
+        <q-card-section horizontal>
+          <div class="solicitation-preview">
+            <div style="width: 200px">
+              <h5 class="solicitation-title">
+                Titulo da solicitação
+              </h5>
+            </div>
+            <div class="solicitation-subtitle">
+              <q-badge class="mr-5" color="blue">
+                Frágil
+              </q-badge>
+              <q-badge color="blue">
+                Grande
+              </q-badge>
+            </div>
+            <div class="locale-container" style="margin-top: 20px">
+              <div class="solicitation-locale"> De: Jaraguá do Sul</div>
+              <div class="solicitation-locale"> Para: Jaraguá do Sul</div>
+              <div class="solicitation-locale">
+                <q-badge class="mr-5" color="blue">
+                  5 Km
+                </q-badge>
+              </div>
+            </div>
+          </div>
+          <q-card-actions vertical class="justify-around q-px-md">
+            <q-btn flat round color="red" icon="done" />
+            <q-btn flat round color="accent" icon="clear" />
+            <q-btn flat round color="primary" icon="question_answer" />
+          </q-card-actions>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  computed: {
+    list() {
+      return ['a', 'b', 'a', 'b', 'a', 'b', 'a', 'b'];
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  .solicitation-preview {
+    padding: 20px;
+    .solicitation-title {
+      font-size: 18px;
+    }
+    .solicitation-subtitle {
+      font-size: 12px;
+    }
+    .solicitation-locale {
+      font-size: 12px;
+      font-weight: 500;
+    }
+  }
+</style>
